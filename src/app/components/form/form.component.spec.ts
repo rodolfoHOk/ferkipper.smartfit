5ce57cd4bca0f61a1022765a786d4647797bf252
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-27 17:04:39 -0300

    test: add tests in form component test

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -4,10 +4,13 @@ import { FormComponent } from './form.component';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,10 +20,32 @@ describe('FormComponent', () => {
     });
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSubmit when the find button is clicked', () => {
+    spyOn(component, 'onSubmit');
+    const findButtonElement = debugElement.query(By.css('#find-button'));
+    findButtonElement.nativeElement.click();
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should emit submitEvent when the find button is clicked', () => {
+    spyOn(component.submitEvent, 'emit');
+    const findButtonElement = debugElement.query(By.css('#find-button'));
+    findButtonElement.nativeElement.click();
+    expect(component.submitEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should call onClean when the clear button is clicked', () => {
+    spyOn(component, 'onClean');
+    const clearButtonElement = debugElement.query(By.css('#clear-button'));
+    clearButtonElement.nativeElement.click();
+    expect(component.onClean).toHaveBeenCalled();
   });
 });
